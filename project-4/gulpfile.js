d613c6b3f14cb7f302d5492de61d2cf440e7760a
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-01-16 17:11:10 +0300

    project-4 added

---
+++ project-4/gulpfile.js
@@ -0,0 +1,90 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+const minifyCSS = require('gulp-clean-css')
+const minifyImg = require('gulp-imagemin')
+const minifyJS = require('gulp-uglify')
+const concat = require('gulp-concat')
+const minifyHTML = require('gulp-htmlmin')
+const autoprefixer = require('gulp-autoprefixer')
+const del = require('del')
+var reload = browserSync.reload
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  })
+})
+
+gulp.task('css', () => {
+  return gulp
+    .src('src/scss/**/*.scss')
+    .pipe(
+      sass({
+        outputStyle: 'nested',
+        precision: 10,
+        includePaths: ['.']
+      }).on('error', sass.logError)
+    )
+    .pipe(minifyCSS())
+    .pipe(autoprefixer())
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('js', () => {
+  return gulp
+    .src('src/js/**/*.js')
+    .pipe(concat('app.min.js'))
+    .pipe(minifyJS())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('html', () => {
+  return gulp
+    .src('./src/*.html')
+    .pipe(
+      minifyHTML({
+        collapseWhitespace: true
+      })
+    )
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('img', () => {
+  /*
+    Eğer resimleri minify etmek isterseniz yorum satırını kaldırabilirsiniz.
+    */
+  gulp
+    .src('src/img/**/*')
+    // .pipe(minifyImg())
+    .pipe(gulp.dest('dist/img'))
+})
+
+gulp.task('img', () => {
+  gulp
+    .src('src/img/**/*')
+    .pipe(minifyImg())
+    .pipe(gulp.dest('dist/img'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch('src/scss/**/*.scss', gulp.task('css'))
+  gulp.watch('src/assets/js/**/*.js', gulp.series('js'))
+  gulp.watch('src/images/**/*.jpg', gulp.series('img'))
+  gulp.watch('src/**/*.html', gulp.series('html'))
+})
+gulp.task('delete', () =>
+  del(['dist/css', 'dist/js', 'dist/img', 'dist/**/*.html'])
+)
+gulp.task(
+  'default',
+  gulp.series(
+    'delete',
+    gulp.parallel('html', 'css', 'js', 'img', 'browser-sync', 'watch')
+  )
+)
